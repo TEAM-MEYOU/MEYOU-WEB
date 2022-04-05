2912fc86f4af87e5aa72362a0dbbfe8dad956feb
gudwh14 <alxious@naver.com>
2022-04-05 22:46:25 +0900

    feat : Navigation 적용 (#14)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,11 +2,13 @@ import 'styles/globals.css';
 import 'normalize.css/normalize.css';
 import type { AppProps } from 'next/app';
 import Layout from '@components/AppLayout';
+import Navigation from '@components/Navigation';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Layout>
       <Component {...pageProps} />
+      <Navigation />
     </Layout>
   );
 }
