9cd59433b77fc7acfe95eae30bef5d91754dd04c
gudwh14 <alxious@naver.com>
2022-04-07 01:02:28 +0900

    feat : Image 컴포넌트 생성 (#20)

---
+++ src/components/Image.tsx
@@ -0,0 +1,32 @@
+import NextImage from 'next/image';
+import { css } from '@emotion/react';
+
+interface Props {
+  className?: string;
+  width: number;
+  height: number;
+  src: string;
+  radius?: string;
+  alt: string;
+}
+
+function Image(props: Props) {
+  const { className, width, height, src, radius = '50%', alt } = props;
+  return (
+    <span className={className} css={css``}>
+      <NextImage
+        css={css`
+          border-radius: ${radius};
+        `}
+        width={width}
+        height={height}
+        src={src}
+        objectFit={'cover'}
+        alt={alt}
+        layout={'fixed'}
+      />
+    </span>
+  );
+}
+
+export default Image;
