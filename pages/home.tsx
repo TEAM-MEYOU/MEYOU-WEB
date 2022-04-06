6e7fab688816f7e392aa3f2a715d1bc1064500ec
gudwh14 <alxious@naver.com>
2022-04-07 01:25:38 +0900

    feat : Home 페이지 서버사이드 삭제

--- pages/home.tsx
+++ pages/home.tsx
@@ -1,4 +1,4 @@
-import { GetServerSideProps, NextPage } from 'next';
+import { NextPage } from 'next';
 import Container from '@components/Container';
 import { css } from '@emotion/react';
 import Text from '@components/Text';
@@ -66,12 +66,6 @@ const Home: NextPage = () => {
       </ColumnList>
     </>
   );
-};
-
-export const getServerSideProps: GetServerSideProps = async () => {
-  return {
-    props: {},
-  };
 };
 
 export default Home;
