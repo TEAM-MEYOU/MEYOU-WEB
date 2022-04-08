6a328ae6b328c297c2057057973232a40df64bc7
gudwh14 <alxious@naver.com>
2022-04-09 01:23:10 +0900

    feat : kakao login 구현 (#26)

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,21 +3,69 @@ import styled from '@emotion/styled';
 import Text from '@components/Text';
 import { css } from '@emotion/react';
 import colors from '@constants/colors';
-import Button from '@components/Button';
+import Script from 'next/script';
 import { useRouter } from 'next/router';
+
+interface login {
+  access_token: string;
+  expires_in: number;
+  refresh_token: string;
+  refresh_token_expires_in: number;
+  scope: string;
+  token_type: string;
+}
+
+interface kakaoInfo {
+  connected_at: string;
+  id: number;
+  kakao_account: {
+    email: string;
+    email_needs_agreement: boolean;
+    has_email: boolean;
+    is_email_valid: boolean;
+    is_email_verified: boolean;
+  };
+}
 
 const Index: NextPage = () => {
   const router = useRouter();
 
   const handleClickKaKaoLogin = () => {
-    router.push('/home');
-    /*
-    todo
-    kakao oauth login logic
-     */
+    const kakao = (window as any).Kakao;
+    if (!kakao.isInitialized()) {
+      kakao.init(process.env.NEXT_PUBLIC_KAKAO_KEY);
+
+      kakao.Auth.login({
+        scope: 'account_email',
+        success: function (response: login) {
+          kakao.Auth.setAccessToken(response.access_token);
+          kakao.API.request({
+            url: '/v2/user/me',
+            data: {
+              property_keys: ['kakao_account.email'],
+            },
+            success: async function (response: kakaoInfo) {
+              const id = response.id;
+              const email = response.kakao_account.email;
+              window.localStorage.setItem('id', id.toString());
+              window.localStorage.setItem('email', email);
+              router.push('/getting-started');
+            },
+            fail: function (error: Error) {
+              console.log(error);
+            },
+          });
+        },
+        fail: function (error: Error) {
+          console.log(error);
+        },
+      });
+    }
   };
   return (
     <>
+      <Script src={'https://developers.kakao.com/sdk/js/kakao.js'} strategy={'beforeInteractive'} />
+
       <Image src={'/icons/app_icon.png'} alt={'로고'} />
       <Text
         css={css`
@@ -33,16 +81,18 @@ const Index: NextPage = () => {
         `}>
         둘이서 꾸며가는 우리들만의 이야기
       </Text>
-      <Button
+      <img
         onClick={handleClickKaKaoLogin}
-        fullWidth={false}
-        color={colors.black}
+        src={'icons/kakao_login_large_narrow.png'}
         css={css`
-          background-color: ${colors.yellow500};
+          width: 180px;
+          background-size: cover;
           margin-top: 200px;
-        `}>
-        카카오계정으로 시작하기
-      </Button>
+          border-radius: 12px;
+          cursor: pointer;
+        `}
+        alt={'카카오 로그인 이미지'}
+      />
     </>
   );
 };
