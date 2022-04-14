54e7ab5361b46b865df2952fe5376867cd2952fc
gudwh14 <alxious@naver.com>
2022-04-14 14:47:10 +0900

    feat : 커플 정보 보여주기 (#46)

--- pages/home.tsx
+++ pages/home.tsx
@@ -1,68 +1,29 @@
 import { NextPage } from 'next';
-import Container from '@components/Container';
-import { css } from '@emotion/react';
-import Text from '@components/Text';
-import colors from '@constants/colors';
 import DiaryLog from '@components/DiaryLog';
 import ColumnList from '@components/ColumnList';
-import Image from '@components/Image';
+import CoupleInfo from '@components/CoupleInfo';
+import { useEffect, useState } from 'react';
+import Profile from '@components/setting/Profile';
+import useUser from '@hooks/useUser';
 
 const Home: NextPage = () => {
+  const [modal, setModal] = useState(false);
+  const user = useUser();
+
+  useEffect(() => {
+    if (user.data) {
+      user.data.coupleInfo!.nickname || setModal(true);
+    }
+  }, [user]);
   return (
     <>
-      <Container
-        css={css`
-          display: flex;
-          flex-direction: column;
-          align-items: center;
-        `}>
-        <Image
-          css={css`
-            position: absolute;
-            left: 10px;
-          `}
-          width={60}
-          height={60}
-          src={'/icons/profile_mock_img_1.jpeg'}
-          alt={'프로필이미지'}
-        />
-        <Image
-          css={css`
-            position: absolute;
-            right: 10px;
-          `}
-          width={60}
-          height={60}
-          src={'/icons/profile_mock_img_2.jpeg'}
-          alt={'프로필이미지'}
-        />
-        <Text
-          css={css`
-            font-size: 3rem;
-          `}>
-          2020.05.05
-        </Text>
-        <Text
-          css={css`
-            font-size: 3rem;
-            color: ${colors.content300};
-          `}>
-          +700 연애중
-        </Text>
-        <Text
-          css={css`
-            font-size: 3rem;
-            color: ${colors.content100};
-            margin-top: 5px;
-          `}>
-          오늘의 감정을 남겨보세요
-        </Text>
-      </Container>
+      <CoupleInfo />
       <ColumnList>
         <DiaryLog date={{ year: 2022, month: 4, day: 11 }} />
         <DiaryLog date={{ year: 2022, month: 4, day: 10 }} />
         <DiaryLog date={{ year: 2022, month: 4, day: 9 }} />
       </ColumnList>
+      {modal && <Profile onClose={() => setModal(false)} />}
     </>
   );
 };
