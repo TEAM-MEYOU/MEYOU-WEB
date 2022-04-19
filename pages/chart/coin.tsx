f3820350e1439f0daa10b959935602b22caf9b21
gudwh14 <alxious@naver.com>
2022-04-19 14:21:57 +0900

    feat : 코인 사용로그 페이지 (#55)

---
+++ pages/chart/coin.tsx
@@ -0,0 +1,84 @@
+import { NextPage } from 'next';
+import Header from '@components/Header';
+import { useRouter } from 'next/router';
+import Text from '@components/Text';
+import ColumnList from '@components/ColumnList';
+import { css } from '@emotion/react';
+import { useQuery } from 'react-query';
+import { CoinGainHash, CoinLog, CoinLogHash, getCoinLog } from '@apis/coin';
+import useUser from '@hooks/useUser';
+import { ToJavaLocaleDate } from '@utils/date';
+import colors from '@constants/colors';
+import Image from '@components/Image';
+import { memo } from 'react';
+
+const todayDate = new Date();
+const today = ToJavaLocaleDate(todayDate);
+const monthAgoDate = new Date();
+monthAgoDate.setMonth(monthAgoDate.getMonth() - 1);
+const monthAgo = ToJavaLocaleDate(monthAgoDate);
+
+const Coin: NextPage = () => {
+  const router = useRouter();
+  const user = useUser();
+  const coinLog = useQuery<Array<CoinLog>>(
+    ['coin', monthAgo, today],
+    () => getCoinLog(user.data!.coupleId, monthAgo, today),
+    {
+      enabled: user.data !== undefined,
+    }
+  );
+  return (
+    <>
+      <Header onClick={() => router.back()}>코인 사용 기록</Header>
+      <ColumnList>
+        {coinLog.data?.map(log => (
+          <CoinLogItem log={log} key={log.id} />
+        ))}
+      </ColumnList>
+    </>
+  );
+};
+
+interface Props {
+  log: CoinLog;
+}
+
+const CoinLogItem = memo(({ log }: Props) => {
+  return (
+    <div
+      key={log.id}
+      css={css`
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        width: 90%;
+        padding: 1rem;
+        border-radius: 12px;
+        background-color: ${log.coinLogType === 'GAIN' ? colors.blue200 : colors.content100};
+        box-shadow: ${colors.boxShadow};
+      `}>
+      <Text>{new Date(log.createdDate).toLocaleString()}</Text>
+      <Text>{CoinLogHash[log.coinLogType]}</Text>
+      <Text>{CoinGainHash[log.coinGainType]}</Text>
+      <div
+        css={css`
+          display: flex;
+          align-items: center;
+        `}>
+        <Image width={20} height={20} src={'/icons/coin.png'} alt={'코인 아이콘'} />
+        <Text
+          css={css`
+            margin-left: 0.5rem;
+            font-size: 2.5rem;
+          `}>
+          {log.coinQuantity}
+        </Text>
+      </div>
+    </div>
+  );
+});
+
+CoinLogItem.displayName = 'CoinLogItem';
+
+export default Coin;
